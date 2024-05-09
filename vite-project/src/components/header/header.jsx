bb4a8fa347f455797f0f1fd88edfbf5b22ff7fd9
anguseva25 <annaguseva97@gmail.com>
2024-05-09 18:10:03 +0300

    added a component "popUser.jsx"

--- vite-project/src/components/header/header.jsx
+++ vite-project/src/components/header/header.jsx
@@ -1,3 +1,5 @@
+import PopUser from "../popUser/PopUser.jsx";
+
 const Header = () => {
     return (<header className="header">
         <div className="container">
@@ -12,7 +14,7 @@ const Header = () => {
                     <button className="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать
                         новую задачу</a></button>
                     <a href="#user-set-target" className="header__user _hover02">Ivan Ivanov</a>
-                    <div className="header__pop-user-set pop-user-set" id="user-set-target">
+                    {/*<div className="header__pop-user-set pop-user-set" id="user-set-target">
                         <p className="pop-user-set__name">Ivan Ivanov</p>
                         <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
                         <div className="pop-user-set__theme">
@@ -20,7 +22,8 @@ const Header = () => {
                             <input type="checkbox" className="checkbox" name="checkbox"/>
                         </div>
                         <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
-                    </div>
+                    </div>*/}
+                    <PopUser/>
                 </nav>
             </div>
         </div>
