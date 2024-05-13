6652a33c002b57ed6c9e6dd2d504faede0696a73
anguseva25 <annaguseva97@gmail.com>
2024-05-13 22:46:07 +0300

    added onClick on PopUser in Header.jsx

--- vite-project/src/components/header/header.jsx
+++ vite-project/src/components/header/header.jsx
@@ -1,6 +1,17 @@
 import PopUser from "../popUser/PopUser.jsx";
+import {useEffect, useState} from "react";
 
 const Header = () => {
+    const [isOpen, setOpen] = useState(false);
+
+    const manualOpen = () => {
+        setOpen((prev) => !prev);
+    };
+
+    useEffect(() => {
+        console.log(isOpen)
+    }, [isOpen]);
+
     return (<header className="header">
         <div className="container">
             <div className="header__block">
@@ -13,7 +24,8 @@ const Header = () => {
                 <nav className="header__nav">
                     <button className="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать
                         новую задачу</a></button>
-                    <a href="#user-set-target" className="header__user _hover02">Ivan Ivanov</a>
+                    <div className="header__user _hover02" onClick={manualOpen}>Ivan Ivanov</div>
+                    {isOpen && (<PopUser/>)}
                     {/*<div className="header__pop-user-set pop-user-set" id="user-set-target">
                         <p className="pop-user-set__name">Ivan Ivanov</p>
                         <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
@@ -23,7 +35,6 @@ const Header = () => {
                         </div>
                         <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
                     </div>*/}
-                    <PopUser/>
                 </nav>
             </div>
         </div>
