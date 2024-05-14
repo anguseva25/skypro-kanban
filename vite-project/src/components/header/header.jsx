3d804dfdc08f530e3c1873d3e7b7b5df416e34e4
anguseva25 <annaguseva97@gmail.com>
2024-05-14 22:43:28 +0300

    added newCards, color for topics, loading status

--- vite-project/src/components/header/header.jsx
+++ vite-project/src/components/header/header.jsx
@@ -1,16 +1,28 @@
 import PopUser from "../popUser/PopUser.jsx";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
-const Header = () => {
+const Header = ({setCards, cards}) => {
     const [isOpen, setOpen] = useState(false);
 
     const manualOpen = () => {
         setOpen((prev) => !prev);
     };
 
-    useEffect(() => {
+    {/*useEffect(() => {
         console.log(isOpen)
-    }, [isOpen]);
+    }, [isOpen]);*/}
+
+    const addNewCard = () => {
+        const newCard = {
+            id: Date.now(),
+            title: "my Test",
+            topic: "Copywriting",
+            date: "12.05.2024",
+            status: "Без статуса"
+        }
+        const newCardList = [...cards, newCard];
+        setCards(newCardList);
+    }
 
     return (<header className="header">
         <div className="container">
@@ -22,8 +34,8 @@ const Header = () => {
                     <a href="" target="_self"><img src="images/logo_dark.png" alt="logo"/></a>
                 </div>
                 <nav className="header__nav">
-                    <button className="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать
-                        новую задачу</a></button>
+                    <button className="header__btn-main-new _hover01" onClick={addNewCard}>
+                        Создать новую задачу</button>
                     <div className="header__user _hover02" onClick={manualOpen}>Ivan Ivanov</div>
                     {isOpen && (<PopUser/>)}
                     {/*<div className="header__pop-user-set pop-user-set" id="user-set-target">
